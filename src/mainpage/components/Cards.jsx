4f92749a8f145815899628b2d1e385a724ca8147
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-01-15 18:20:30 +0400

    file added

--- src/mainpage/components/Cards.jsx
+++ src/mainpage/components/Cards.jsx
@@ -10,7 +10,8 @@ export default function Cards() {
               <p className='my-4 text-justify font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur nihil possimus harum facere nesciunt tempore nobis illo enim adipisci repudiandae non voluptatem unde aperiam, deserunt odio alias pariatur iusto error numquam dolorum! Quasi id assumenda exercitationem quae enim, tempore amet porro quod, pariatur, nesciunt a rem libero nostrum aliquam dolorum!</p>
             </div>
             <div className='flex bg-zinc-300 justify-end h-[20%] items-center'>
-              <button className='bg-yellow-400 py-2 px-5 rounded-[5px] font-bold hover:bg-yellow-500'>Edit</button>
+              <button
+                className='bg-yellow-400 py-2 px-5 rounded-[5px] font-bold hover:bg-yellow-500'>Edit</button>
               <button className='bg-yellow-400 py-2 px-5 rounded-[5px] font-bold mx-2 hover:bg-yellow-500'>Delete</button>
             </div>
     </div>
