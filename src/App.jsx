33e7b862aed231d8e31acd0862b565ee8ce3ad71
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-01-16 14:56:35 +0400

    Edit,Create,Delete views completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,5 +6,5 @@ import { useState } from 'react'
 export default function App() {
   const [authorized,setAuthorized] = useState(false)
   const [mail,setMail] = useState('')
-  return authorized ? <Mainpage setAuthorized={setAuthorized} mail = {mail}/> : <Login setAuthorized = {setAuthorized} mail = {mail} setMail={setMail}/> 
+  return authorized ? <Mainpage setAuthorized={setAuthorized} mail = {mail}/> : <Login setAuthorized = {setAuthorized} mail = {mail} setMail={setMail}/>
 }
--- src/mainpage/components/DeleteCard.jsx
+++ src/mainpage/components/DeleteCard.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
 export default function DeleteCard() {
   return (
-    <div>DeleteCard</div>
-  )
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <div className='mx-auto bg-slate-100 w-[700px] h-[241px] rounded-[13px]'>
+        <div className="flex justify-end">
+          <button className='bg-red-700 text-slate-100 w-[30px] h-[30px] rounded-full mt-2 mr-2'>X</button>
+        </div>
+        <h1 className='font-bold flex justify-center m-2 uppercase text-[30px]'>Delete Card</h1>
+        <div className="flex justify-center">
+          <p className="text-[20px]">Are you sure you want to delete card "..."?</p>
+        </div>
+        <div className='flex justify-center text-[20px]'>
+          <button className='bg-slate-200 w-[124px] h-[61px] rounded-[15px] m-5'>Close</button>
+          <button className='bg-yellow-400 w-[124px] h-[61px] rounded-[15px] m-5'>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 }
